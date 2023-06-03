2190c6441b217e6fb82d7f6eb490a54f577ab586
Thinesan Sivapathasundaram <sivapthi@students.zhaw.ch>
2023-06-04 01:53:33 +0200

    Präsentation Test

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const HOST = '0.0.0.0';
 // App
 const app = express();
 app.get('/', (req, res) => {
-	res.send('Hello DevOps FS23 Course! Best regards, sivapthi :-)');
+	res.send('Hello DevOps FS23 Course! Best regards, Test :-)');
 });
 
 app.listen(PORT, HOST);
